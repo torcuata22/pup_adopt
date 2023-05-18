d1cae067ea49bdb962a3b800ccd4cb084bc40539
torcuata22 <marilynjmarquez@gmail.com>
2023-05-18 10:52:18 -0500

    started

--- adoption_site.py
+++ adoption_site.py
@@ -1,12 +1,12 @@
 import os
-#from forms import AddForm, DeleteForm
+from forms import AddForm, DelForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.congif['SECRET_KEY']='mysecretekey'
+app.config['SECRET_KEY']='mysecretekey'
 
 #SQL Database (for larger project use models.py)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -28,3 +28,48 @@ class Puppy(db.Model):
     def __repr__(self):
         return f"Puppy name: {self.name}"
     
+#view functions (they use forms):
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/add', methods = ['GET','POST'])
+def add_pup():
+    form = AddForm()
+    if form.validate_on_submit():
+        name = form.name.data 
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    
+    return render_template('add.html')
+
+@app.route('/list')
+def list_pup():
+    puppies = Puppy.query.all()
+    return render_template('list.html', puppies=puppies)
+
+@app.route('/delete', methods=['GET','POST'])
+def del_pup():
+    form = DelForm
+    if form.validate_on_submit():
+        id = form.id.data("Remove Puppy")
+        pup = Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.comit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('delete.html', form = form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#create flask app:  export FLASK_APP=adoption_site.py
+#initialize db: flask db init 
+#flask db migrate -m "new tables"
+#flask db upgrade
---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+
+class AddForm(FlaskForm):
+    name = StringField('Puppy Name: ')
+    submit=SubmitField('Add Puppy')
+
+class DelForm(FlaskForm):
+    id=IntegerField("Id number of Puppy to remove")
+    submit = SubmitField("Remove Puppy")
