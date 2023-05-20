7c17a222ca370b2c2a67dfd7df888ef8350ebd07
torcuata22 <marilynjmarquez@gmail.com>
2023-05-20 12:36:58 -0500

    refactoring part one

--- adoption_site.py
+++ adoption_site.py
@@ -17,35 +17,7 @@ db = SQLAlchemy(app)
 Migrate(app,db)
 
 #Models:
-class Puppy(db.Model):
-    __tablename__ = 'puppies'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.Text)
-    #1:1 with owner:
-    owner = db.relationship('Owner',backref='puppy',uselist=False)
 
-    def __init__(self,name):
-        self.name = name
-
-    def __repr__(self):
-        if self.owner:
-            return f"Puppy name: {self.name}, and their owner is:{self.owner.name}"
-        else:
-            return f"Puppy name: {self.name} and they're still looking for a home"
-    
-class Owner(db.Model):
-    __tablename__ = "owners"
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.Text)
-    #1:1 with puppy:
-    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
-
-    def __init__(self, name, puppy_id):
-        self.name = name
-        self.puppy_id = puppy_id
-
-    def __repr__(self):
-        return f"Owner name: {self.name}"
 
 
 
@@ -112,3 +84,11 @@ if __name__ == '__main__':
 #initialize db: flask db init 
 #flask db migrate -m "new tables"
 #flask db upgrade
+
+#blueprints library wil register url prefix for each views.py file when we refactor
+#/owners/add and /puppies/add
+#Refactor:
+#Restructure project folders
+#Add blueprints
+#register Blueprints in __init__.py
+# __init__.py contains all the information that creates the application
